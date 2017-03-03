3c52f9e15e7009a546530e1332d3c25fb3dea459
Nabeel Khan <mnkhan@fullsail.edu>
2017-03-02 19:34:24 -0800

    Run the server on 3000

--- src/server.js
+++ src/server.js
@@ -7,3 +7,12 @@ const app = express();
 
 // Config the localhost port location
 const port = 3000;
+
+// Connect Body-Parser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// Start the server 
+const server = app.listen(port, function() {
+	  console.log('The Server is running on port ' + port, "success");
+});
