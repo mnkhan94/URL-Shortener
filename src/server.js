017b5eb569b59b0320bf42cb2f558fe814f3947c
Nabeel Khan <mnkhan@fullsail.edu>
2017-03-02 19:31:45 -0800

    Created An Express APP and pushed it to the port 3000

--- src/server.js
+++ src/server.js
@@ -1,2 +1,9 @@
 // Require Express
 const express = require('express');
+const bodyParser = require('body-parser')
+
+// Create an app using Express
+const app = express();
+
+// Config the localhost port location
+const port = 3000;
