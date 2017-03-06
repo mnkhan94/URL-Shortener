7c7d187b05f62762c044bf6d8c5c0b3d2cbce866
Nabeel Khan <mnkhan@fullsail.edu>
2017-03-05 20:20:24 -0800

    Successfully connected to database using sequel and dotenv

--- models/db.js
+++ models/db.js
@@ -6,3 +6,17 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
+// Use Sequelize To Connect To Database
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_SCHEMA,
+  port: process.env.DB_PORT,
+  // How many connections are happening?
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
+  },
+  // Prevent overloading the database
+  logging: false,
+});
