2d3b390b804db17265fc364087a312a15308955b
Nabeel Khan <mnkhan@fullsail.edu>
2017-03-05 20:47:42 -0800

    Added Read routers in API

--- routes/api/url.js
+++ routes/api/url.js
@@ -13,7 +13,7 @@ module.exports = function (express) {
   // CREATE A Shortened URL
   router.post('/url', (req, res) => {
     
-    // Create an Alphanumeric String
+  // Create an Alphanumeric String
     url_short = randomstring.generate(6);
     req.body.short_url = url_short;
     
@@ -23,6 +23,31 @@ module.exports = function (express) {
       res.status(200).json(data);
     });
   });
+
+
+  // READ / Get all Shortened URLS
+  router.get("/urls", (req, res) => {
+    url.findAll((err) => {
+      utility.debug('Unable to get all Shortened URLs', "error");
+      res.status(500).json(err);
+    }, (data) => {
+      utility.debug('Successfully retrieved all Shortened URLs', "success");
+      res.status(200).json(data);
+    })
+  });
+
+  // READ Specific Shortened URL by ID
+  router.get("/urls/:id", (req, res) => {
+    req.body.id = req.params.id;
+    url.findID(req.body, (err) => {
+      utility.debug('Unable to find Shortened URL', "error");
+      res.status(500).json(err);
+    }, (data) => {
+      utility.debug('Successfully updated Shortened URL', "success");
+      res.status(200).json(data);
+    })
+  });
+
 
   return router;
 
