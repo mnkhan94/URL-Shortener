51536521772b878239c7b5002904742cd3d29cb2
Nabeel Khan <mnkhan@fullsail.edu>
2017-03-05 20:46:58 -0800

    Added Post router in Api

--- routes/api/url.js
+++ routes/api/url.js
@@ -1,18 +1,29 @@
-module.exports = function(express) {
-    const router = express.Router();
-    // Package for Random Alphanumeric String
-    const randomstring = require('randomstring')
+const url = require("../../models/url");
 
-    // Post 
-    router.post('/v1/urls', (req, res) => {
-        
-        // Create an Alphanumeric String
-        url_short = randomstring.generate(6);
+// Export Express From Index.js
 
-        // Return The String
-        res.status(200).json(url_short);
+module.exports = function (express) {
 
+  // Use Express' Router
+  const router = express.Router();
+
+  // Package for Random Alphanumeric String
+  const randomstring = require('randomstring')
+
+  // CREATE A Shortened URL
+  router.post('/url', (req, res) => {
+    
+    // Create an Alphanumeric String
+    url_short = randomstring.generate(6);
+    req.body.short_url = url_short;
+    
+    url.create(req.body, (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
     });
+  });
 
-    return router
-}
+  return router;
+
+};
