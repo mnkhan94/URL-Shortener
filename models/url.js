f0d469cf72b0693c1afc7f099735dac5d3fb96a4
Nabeel Khan <mnkhan@fullsail.edu>
2017-03-05 20:25:12 -0800

    Added Create functionality in the URL model

--- models/url.js
+++ models/url.js
@@ -1,2 +1,10 @@
 // This is the URL Model. 
 // The CRUD functionalities of the Shortened Urls will live in here
+
+// Require the DB as a dependency
+const db = require("./db");
+
+/* ~ CREATE ~ */
+exports.create = (payload, err, success) => {
+	db.url.create(payload).then(success).catch(err);
+};
