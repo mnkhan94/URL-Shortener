ddb672d7fbb8e206b9649b6fbead1b98edb46a07
Nabeel Khan <mnkhan@fullsail.edu>
2017-03-05 20:37:29 -0800

    Added Read functions in the URL model

--- models/url.js
+++ models/url.js
@@ -8,3 +8,21 @@ const db = require("./db");
 exports.create = (payload, err, success) => {
 	db.url.create(payload).then(success).catch(err);
 };
+
+/* ~ READ ~ */
+exports.findAll = (err, success) => {
+  	db.url.findAll().then(success).catch(err);
+};
+
+/* ~ READ Shortened Url by ID ~ */
+exports.findID = (payload, err, success) => {
+  db.url.find({
+    where: {
+      id: payload.id,
+    },
+    include: [{
+      all: true,
+      nested: true,
+    }]
+  }).then(success).catch(err);
+};
