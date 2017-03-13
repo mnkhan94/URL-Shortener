931619f0120f420d37436584c658480fa6c18463
Nabeel Khan <mnkhan@fullsail.edu>
2017-03-12 17:43:03 -0700

    Styled the index.js according to the code-style

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,52 +1,53 @@
-// This is the index page! This holds the functions for what will happen when the user first visits the homepage.
+// This is the index page!
+// This holds the functions for what will happen when the user first visits the homepage.
 
 // Import URLs model
-const url = require("../models/url");
+const url = require('../models/url');
+const utility = require('../lib/debug');
 
 // Export this module, so that server.js can import it
-module.exports = function (express) {
+module.exports = (express) => {
+  // Use Express to define routers
+  const router = express.Router();
 
-	// Use Express to define routers
-	const router = express.Router();
-
-	// For the initial page of the API, welcome the user
-	router.get('/', function(req, res) {
-		res.json(
-			{
-				Hi: "Welcome to the short.io URL-Shortener",
-				GET: {
-					"/": "Will take you to the Welcome Screen",
-					"/go/:shortUrl": "Will redirect you to the Original Url",
-					"/api/v1/urls": "Returns back all URLs in database'"
-				},
-				POST: {
-					"/api/v1/url": "Saves a new shortened url like 'short.io/v97fhg'",
-					"/api/v1/url/:id": "Finds & Updates the specified shortened Url"
-				},
-				DELETE: {
-					"/api/v1/url/:id": "Finds & Deletes the specified shortened Url",
-				}
-			}
-		);
-
-	});
+  // For the initial page of the API, welcome the user
+  router.get('/', (req, res) => {
+    res.json(
+      {
+        Hi: 'Welcome to the short.io URL-Shortener',
+        GET: {
+          '/': 'Will take you to the Welcome Screen',
+          '/go/:shortUrl': 'Will redirect you to the Original Url',
+          '/api/v1/urls': 'Returns back all URLs in database',
+        },
+        POST: {
+          '/api/v1/url': 'Saves a new shortened url like short.io/v97fhg',
+          '/api/v1/url/:id': 'Finds & Updates the specified shortened Url',
+        },
+        DELETE: {
+          '/api/v1/url/:id': 'Finds & Deletes the specified shortened Url',
+        },
+      },
+    );
+  });
 
 
-	router.get("/go/:shortURL", (req, res) => {
-		req.body.short_url = req.params.shortURL;
-		url.findShortURL(req.body, (err) => {
-			utility.debug('URL not found!', "error");		  
-			res.status(500).json(err);
-		}, (data) => {
-			utility.debug('Redirecting to ' + "http://www." + data.original_url, "success");
-			res.redirect("http://www." + data.original_url);
-		})
-	});
+  router.get('/go/:shortURL', (req, res) => {
+    const request = req;
+    request.body.short_url = req.params.shortURL;
+    url.findShortURL(req.body, (err) => {
+      utility.debug('URL not found!', 'error');
+      res.status(500).json(err);
+    }, (data) => {
+      utility.debug('Redirecting to http://www.' + data.original_url, 'success');
+      res.redirect('http://www.' + data.original_url);
+    });
+  });
 
-	// Import API Routes
-	router.use("/api/v1", require("./api/url.js")(express));
+  // Import API Routes
+  router.use('/api/v1', require('./api/url.js')(express));
 
 
-	// Return The Built Router
-	return router;
-}
+  // Return The Built Router
+  return router;
+};
